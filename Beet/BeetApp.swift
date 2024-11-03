3f2e3172601f09eac6561035b94ea5918a7d80df
alfr-ruiz <141072839+alfr-ruiz@users.noreply.github.com>
2024-11-03 05:35:10 -0500

    Initial Commit

---
+++ Beet/BeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeetApp.swift
+//  Beet
+//
+//  Created by Alfredo Ruiz-Malca on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeetTests/BeetTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeetTests.swift
+//  BeetTests
+//
+//  Created by Alfredo Ruiz-Malca on 11/3/24.
+//
+
+import Testing
+@testable import Beet
+
+struct BeetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
